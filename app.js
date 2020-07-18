4002e3903a0038703a8567d2d89339b8452054eb
ishikawa tatsuro <ishitatsu820@gmail.com>
2020-07-19 00:32:52 +0900

    削除、編集、検索機能実装

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ $('.js-add-todo').on('click', function(e){
 
   $('.js-toggle-error').hide();
 
-  var listItem = '<li class="list__item js-todo_list_item" data-text="' + text + '">' +
+  var listItem = '<li class="list__item js-todo_list-item" data-text="' + text + '">' +
   '<i class="far fa-square icon-check js-click-done" aria-hidden="true"></i>' + 
   '<span class="js-todo_list-text">' + text + '</span>' +
   '<input type="text" class="editText js-todo_list-editForm" value="' + text + '">' +
@@ -38,4 +38,73 @@ $('.js-add-todo').on('click', function(e){
 
   $('.js-todo_list').prepend(listItem);
 
+});
+
+//【2. TODOタスクのアイコンを押下した際にタスクをDONEにする】
+// 1. TODOタスクのアイコンを押下した際にイベントを発火
+// 2. クリックしたDOM（アイコン）をdoneのアイコンに変更
+// 3. クリックしたDOM（アイコン）にjs-click-doneのクラス名を削除し、js-click-todoのクラス名をつける
+// 4. クリックしたDOM（アイコン）から辿って、list__itemのDOMを取得
+// 5. list__itemのクラス名をdoneのものに変更する
+$(document).on('click', '.js-click-done', function(){
+  $(this).addClass('fa-check-square').removeClass('fa-square').removeClass('js-click-done').addClass('js-click-todo').closest('.js-todo_list-item').addClass('list__item--done');
+});
+
+//【3. DONEタスクのアイコンを押下した際にタスクをTODOタスクに戻す】
+// 1. DONEタスクのアイコンを押下した際にイベントを発火
+// 2. クリックしたDOM（アイコン）をtodoのアイコンに変更
+// 3. クリックしたDOM（アイコン）にjs-click-doneのクラス名を追加し、js-click-todoのクラス名を削除する
+// 4. クリックしたDOM（アイコン）から辿って、list__itemのDOMを取得
+// 5. list__itemのクラス名をtodoのものに変更する
+
+$(document).on('click', '.js-click-todo', function(){
+  $(this).addClass('fa-square').removeClass('fa-check-square')
+  .addClass('js-click-done').removeClass('js-click-todo').closest('.js-todo_list-item').removeClass('list__item--done');
+});
+
+//【4. ゴミ箱アイコンを押下した際にタスクを削除する】
+// 1. ゴミ箱アイコンを押下した際にイベントを発火
+// 2. クリックしたDOM（アイコン）から辿って、list__itemのDOMを取得し、削除する
+
+$(document).on('click', '.js-click-trash', function(){
+  $(this).closest('.js-todo_list-item').fadeOut('slow', function(){
+    $(this).remove();
+  });
+});
+
+//【5. TODOタスクのテキストをクリックした際に入力できるようにし、修正可能にする】
+// 1. テキストを押下した際にイベントを発火
+// 2. クリックしたDOM（テキスト）を非表示にし、兄弟要素の編集エリアを表示する
+$(document).on('click', '.js-todo_list-text', function(){
+  $(this).hide().siblings('.js-todo_list-editForm').show();
+});
+
+// 3. 編集エリア内でkeyupしたらイベントを発火
+// 4. Shift+Enterを押したら、値をテキストにも反映し、編集エリアは非表示、テキストは表示する
+$(document).on('keyup', '.js-todo_list-editForm', function(e){
+
+  if(e.keyCode === 13 && e.shiftKey === true){
+    var $this = $(this);
+    $this.hide().siblings('.js-todo_list-text').text($this.val()).show()
+    .closest('.js-todo_list-item').attr('data-text', $this.val());
+  }
+});
+
+
+//【6. 検索エリアに値を入力した際にタスクの中から値にマッチするタスクのみ表示させる】
+// 1. 検索エリアに入力があった際にイベントを発火
+// 2. 全てのlist__itemのDOMを取得し、一つ一つループで展開
+// 3. 値にマッチするもの以外を非表示にする
+$('.js-search').on('keyup', function(){
+  var searchText  = $(this).val();
+
+  $('.js-todo_list-item').show().each(function (i, elm) {
+    var text = $(elm).data('text');
+
+    if( text && text.match(new RegExp('^' + searchText)) ){
+      return true;
+    }
+    $(elm).hide();
+
+  });
 });
